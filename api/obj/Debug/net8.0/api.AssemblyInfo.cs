e993d10d5cdd5ac9dfb52f52a8103f36586b1b3a
emir kaya <emir kaya@DESKTOP-CM0RVOG>
2025-05-17 23:12:52 +0300

    feat: Implement RTK for state management

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a8cc79b89dd1e54e4c00162f8d00a18bd450ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c42767431751a1678b8da5b26932ce1e5c3dbd58")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
